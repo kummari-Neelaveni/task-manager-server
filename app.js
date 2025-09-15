4c6e83f10522876b544b4638b2d4ae64a6edeab6
Kummari-Neelaveni <kummarineelaveni7@gmail.com>
2025-09-15 17:46:04 +0530

    newcors

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ connectDatabase();
 
 app.use(
   cors({
-    origin: ["https://task-manager-client-sable.vercel.app/","http://localhost:5173", "https://your-frontend-domain.onrender.com",],
+    origin: ["https://task-manager-client-sable.vercel.app","http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
